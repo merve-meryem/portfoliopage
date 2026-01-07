0e2f4f842b4a64ebb21064039eca02a443c95197
merve-meryem <meryemkoyuncu@icloud.com>
2026-01-07 15:22:29 +0300

    Link fiixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,46 @@
-import React from 'react'
-import home from "../assets/home.jpeg"
-import {motion} from "framer-motion"
+import React from "react";
+import home from "../assets/home.jpeg";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 function Home() {
   return (
-    <motion.div className='h-full' initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>
-    <div className=' flex mx-50 mt-30 flex-col lg:flex-row px-4 sm:px-8 md:px-12 xl:px-48'>
-      
-      {/* IMAGE CONTAINER */}
-      <div className='relative  lg:h-full lg:w-1/2 '>
-        <img src={home} className='object-contain w-1/2 rounded-full '  />
-      </div>
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className=" flex mx-50 mt-30 flex-col lg:flex-row px-4 sm:px-8 md:px-12 xl:px-48">
+        {/* IMAGE CONTAINER */}
+        <div className="relative  lg:h-full lg:w-1/2 ">
+          <img src={home} className="object-contain w-1/2 rounded-full " />
+        </div>
 
-      {/* TEXT CONTAINER */}
-      <div className='ml-10 relative lg:h-full lg:1-1/2 mt-8 flex flex-col gap-8 items-center text-center'>
-        <h1 className='text-4xl md:text-6xl font-bold'>Merhaba</h1>
-        <p className='md:text-2xl'>I'm, Merve Meryem</p>
-        <span className='md:text-xl'>Frontend Developer</span>
+        {/* TEXT CONTAINER */}
+        <div className="ml-10 relative lg:h-full lg:1-1/2 mt-8 flex flex-col gap-8 items-center text-center">
+          <h1 className="text-4xl md:text-6xl font-bold">Merhaba</h1>
+          <p className="md:text-2xl">I'm, Merve Meryem</p>
+          <span className="md:text-xl">Frontend Developer</span>
 
-        <div className='flex  gap-4'>
-          <a href="/portfolio" className='px-4 py-3 rounded-lg ring-1 ring-black bg-black text-white'>
-            View My Work
-          </a>
-          <a href="/contact" className='px-4 py-3 rounded-lg ring-1 ring-black'>
-            Contact Me
-          </a>
+          <div className="flex  gap-4">
+            <Link
+              to="/portfolio"
+              className="px-4 py-3 rounded-lg ring-1 ring-black bg-black text-white"
+            >
+              View My Work
+            </Link>
+            <Link
+              to="/contact"
+              className="px-4 py-3 rounded-lg ring-1 ring-black"
+            >
+              Contact Me
+            </Link>
+          </div>
         </div>
       </div>
-
-    </div>
     </motion.div>
-  )
+  );
 }
 
-export default Home
+export default Home;
